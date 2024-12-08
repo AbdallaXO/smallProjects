b3a00a69cf8f7455b59192b2273faf5d863f7043
Abdalla <abdi.aly20@gmail.com>
2024-12-08 17:34:47 -0500

    First Commit

---
+++ adieu.py
@@ -0,0 +1,12 @@
+import inflect
+p = inflect.engine()
+names = []
+adieu = "Adieu, adieu, to"
+while True:
+    try:
+        name=input("Name: ").capitalize()
+        names.append(name)    
+    except eof:
+        print(f"{adieu} {p.join(names)}")
+        break
+        
